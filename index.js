9e80e5ba6f9b033f64a453320b18bd859f97b54d
Unknown <pawel.kochanek@gmail.com>
2018-07-08 21:02:29 +0200

    First implementation of listr.

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 #!/usr/bin/env node
 const argv = require('yargs').argv;
 const chalk = require('chalk');
-const gitCommands = require('./gitCommands/handleBranch');
+const execa = require('execa');
+const Listr = require('listr');
+
+const gitCommands = require('./gitCommands');
 const helper = require('./helpers/helpers');
 
 // It retrieve branch name from command line.
-const branch = gitCommands.handleBranch(argv['b']);
+// const branch = gitCommands.handleBranch(argv['b']);
 
 helper.showLogo();
 
-helper
-    .gitPull(branch)
-    .then(res => console.log(chalk.green(res.stdout)))
-    .catch(err => console.log(chalk.white.bgRed(err.stderr)));
+const command = new Listr([
+    {
+        title: 'Checking the branch',
+        task: () =>
+            gitCommands
+                .gitCheckUncommited()
+                .then(res => {
+                    console.log(res);
+                })
+                .catch(err => console.log(err.message)),
+    },
+]);
+
+command.run();
 
 // helper
 //     .gitCheckout(branch)
