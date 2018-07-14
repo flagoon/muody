e2a72bb482a713889d2242cc1b327cd9034f3f99
Unknown <pawel.kochanek@gmail.com>
2018-07-14 23:51:55 +0200

    Helper files.

---
+++ createFolders.js
@@ -0,0 +1,5 @@
+const execa = require('execa');
+
+execa('mkdir', ['dist']);
+execa('mkdir', ['node_modules/avid-nux-pulse-components']);
+execa('mkdir', ['node_modules/avid-uir-crud']);
---
+++ folderToDelete.js
@@ -0,0 +1,5 @@
+module.exports = Object.freeze([
+    './node_modules/avid-nux-pulse-components',
+    './node_modules/avid-uir-crud',
+    'dist',
+]);
